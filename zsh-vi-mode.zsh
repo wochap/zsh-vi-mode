425e4293e243bccf8da3439a25a7699fe4ab3b1b
wochap <gean.marroquin@gmail.com>
2025-02-17 07:22:56 -0500

    perf: apply #304
    apply https://github.com/jeffreytse/zsh-vi-mode/pull/304

--- zsh-vi-mode.zsh
+++ zsh-vi-mode.zsh
@@ -3638,7 +3638,8 @@ function zvm_init() {
 
 # Check if a command is existed
 function zvm_exist_command() {
-  command -v "$1" >/dev/null
+  # command -v "$1" >/dev/null
+  (($+commands[$1] + $+functions[$1]))
 }
 
 # Execute commands
